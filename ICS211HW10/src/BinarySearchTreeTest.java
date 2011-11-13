f96554a7e583bd03ecbdc1c4a44ff94c7eb67a51
fembem <leo.decandia@gmail.com>
2011-11-13 01:30:56 -1000

    added tests for every node in junit fixture.

--- ICS211HW10/src/BinarySearchTreeTest.java
+++ ICS211HW10/src/BinarySearchTreeTest.java
@@ -25,39 +25,94 @@ public class BinarySearchTreeTest {
     
   }
 
+  //d
+  @Test(expected=NoSuchElementException.class)
+  public void testFindNonExistingPrevious() {
+    tree.findPrevious(new Record("d"));
+  }
+  
+  //d
+  @Test
+  public void testFindNextAsParent() {
+    Record next = tree.findNext(new Record("d"));
+    assertEquals(new Record("g").key, next.key);
+  }
+  
+  //g
+  @Test
+  public void testFindNext() {
+    Record next = tree.findNext(new Record("g"));
+    assertEquals(new Record("i").key, next.key);
+  }
+  
+  //g
+  @Test
+  public void testFindPreviousAsLeftChild() {
+    Record next = tree.findPrevious(new Record("g"));
+    assertEquals(new Record("d").key, next.key);
+  }
+  
+  //i
+  @Test
+  public void testFindNextAsParentI() {
+    Record next = tree.findNext(new Record("i"));
+    assertEquals(new Record("j").key, next.key);
+  }
+  
+  //i
+  @Test
+  public void testFindPreviousAsAncestor() {
+    Record next = tree.findPrevious(new Record("i"));
+    assertEquals(new Record("g").key, next.key);
+  }
+  
+  //j
+  @Test
+  public void testFindNextAsRightChild() {
+    Record next = tree.findNext(new Record("j"));
+    assertEquals(new Record("k").key, next.key);
+  }
+  
+  //j
+  @Test
+  public void testFindPreviousAsLeftChildJ() {
+    Record next = tree.findPrevious(new Record("j"));
+    assertEquals(new Record("i").key, next.key);
+  }
+  
+  //k
   @Test
   public void testFindNextThatIsADistantAncestor() {
     Record next = tree.findNext(new Record("k"));
     assertEquals(new Record("m").key, next.key);
   }
   
+  //k
   @Test
   public void testFindPreviousOnRightChildWithNoChildren() {
     Record next = tree.findPrevious(new Record("k"));
     assertEquals(new Record("j").key, next.key);
   }
   
-  @Test
-  public void testFindNextOnNoTargetRecord() {
-    Record next = tree.findNext(new Record("z"));
-    assertNull(next);
-  }
-  
-  @Test
-  public void testFindNext() {
-    Record next = tree.findNext(new Record("g"));
-    assertEquals(new Record("i").key, next.key);
-  }
-
-  @Test(expected=NoSuchElementException.class)
-  public void testFindNonExistingPrevious() {
-    tree.findPrevious(new Record("d"));
-  }
-  
+  //m
   @Test(expected=NoSuchElementException.class)
   public void testFindNextOnRoot() {
     Record next = tree.findNext(new Record("m"));
     System.out.println(next);
+  }
+  
+  //m
+  @Test
+  public void testFindPreviousOnRoot() {
+    Record next = tree.findPrevious(new Record("m"));
+    assertEquals(new Record("k").key, next.key);
+  }
+  
+  //z
+  @Test
+  public void testFindNextOnNoTargetRecord() {
+    Record next = tree.findNext(new Record("z"));
+    assertNull(next);
   }
 
 }
