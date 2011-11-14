de3eab780abb7f4c6d1ebe462c63891e976745dd
fembem <leo.decandia@gmail.com>
2011-11-13 20:09:11 -1000

    fixed error in permutation testing.

--- ICS211HW10/src/BinarySearchTreeTest.java
+++ ICS211HW10/src/BinarySearchTreeTest.java
@@ -4,12 +4,11 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 public class BinarySearchTreeTest {
 
   BinarySearchTree<Record> tree;
-  int N = 8;
-  
+  int N = 3;
+
   @BeforeClass
   public static void setUpBeforeClass() throws Exception {
   }
@@ -23,80 +22,91 @@ public class BinarySearchTreeTest {
     tree.add(new Record("d"));
     tree.add(new Record("k"));
     tree.add(new Record("i"));
-    
+
   }
+
   @Test
   public void testOnTreesOfSizeUpToN() {
-    //generate trees of all size from 1 to N
+    // generate trees of all size from 1 to N
     for (int size = 1; size <= N; size++) {
       Integer[] integerArray = new Integer[size];
-        //put Integers 0..(N-1) into the tree
-        for (int j = 0; j < size; j++) {
-          integerArray[j] = j;
+
+      System.out.println("working on arrays of size :" + size);
+      
+      // put Integers 0..(N-1) into the tree
+      for (int j = 0; j < size; j++) {
+        integerArray[j] = j;
+      }
+      // generate all the permutations of the integers [0..(N-1)]
+      Permute permutations = new Permute(integerArray);
+      // for each permutation add elements to the tree in that order
+      while (permutations.hasNext()) {
+
+        Integer[] array = (Integer[])permutations.next();
+        
+        //provide some feedback on progress
+        System.out.println("working on array :" + permutations);
+
+        // put the Integers into the tree in the order given by the current permutation
+        BinarySearchTree<Integer> integerTree = new BinarySearchTree<Integer>();
+        for (int i = 0; i < size; i++) {
+          integerTree.add(array[i]);
         }
-        //generate all the permutations of the integers [0..(N-1)]
-        Permute permutations = new Permute(integerArray);
-        //for each permutation add elements to the tree in that order
-        while (permutations.hasNext()) {
-          
-          //provide some feedback on progress
-          System.out.println("working on array :" + permutations);
-          
-          //put the Integers into the tree in the order given by the current permutation
-          BinarySearchTree<Integer> integerTree = new BinarySearchTree<Integer>();
-          for (int i = 0; i < size; i++) {
-            integerTree.add(i);
-          }
-          
-          //there is no (size) element in the tree
-          assertNull(integerTree.findNext(size));
-          assertNull(integerTree.findPrevious(size));
-          
-          //for integers between 1 and (size-2) there is both a previous
-          //and a next in the tree
-          for (int i = 1; i < size - 1 ; i++) {
-            Integer next = integerTree.findNext(i);
-            assertEquals(new Integer(i+1), next);
-            Integer previous = integerTree.findPrevious(i);
-            assertEquals(new Integer(i-1), previous);
-          }
-          //for (size-1) there is no next element in the tree
-          try {
-            Integer next = integerTree.findNext(size-1);
-            System.out.println("next(" + (size-1) + ") = " + next);
-            fail();
-          } catch (NoSuchElementException nsee) {
-            //good!
-          }
-          //for 0 there is no previous element in the tree
-          try {
-            Integer next = integerTree.findPrevious(0);
-            fail();
-          } catch (NoSuchElementException nsee) {
-            //good!
-          }
+        
+        //show the tree
+        System.out.println("resulting tree is :" + integerTree);
+
+        // there is no (size) element in the tree
+        assertNull(integerTree.findNext(size));
+        assertNull(integerTree.findPrevious(size));
+
+        // for integers between 1 and (size-2) there is both a previous
+        // and a next in the tree
+        for (int i = 1; i < size - 1; i++) {
+          Integer next = integerTree.findNext(i);
+          assertEquals(new Integer(i + 1), next);
+          Integer previous = integerTree.findPrevious(i);
+          assertEquals(new Integer(i - 1), previous);
         }
+        // for (size-1) there is no next element in the tree
+        try {
+          Integer next = integerTree.findNext(size - 1);
+          System.out.println("next(" + (size - 1) + ") = " + next);
+          fail();
+        }
+        catch (NoSuchElementException nsee) {
+          // good!
+        }
+        // for 0 there is no previous element in the tree
+        try {
+          Integer next = integerTree.findPrevious(0);
+          fail();
+        }
+        catch (NoSuchElementException nsee) {
+          // good!
+        }
+      }
     }
   }
-  
-  //test on tree with only one node
-  @Test(expected=NoSuchElementException.class)
+
+  // test on tree with only one node
+  @Test(expected = NoSuchElementException.class)
   public void testFindNextOnOneNodeTree() {
     tree = new BinarySearchTree<Record>();
     tree.add(new Record("a"));
     Record next = tree.findNext(new Record("a"));
   }
-  
-  //test on tree with two nodes
-  @Test(expected=NoSuchElementException.class)
+
+  // test on tree with two nodes
+  @Test(expected = NoSuchElementException.class)
   public void testFindNextOnTwoNodeTreeNoSuchElement() {
     tree = new BinarySearchTree<Record>();
     tree.add(new Record("a"));
     tree.add(new Record("g"));
     Record next = tree.findNext(new Record("g"));
-  }  
-  
-  //test on tree with two nodes
+  }
+
+  // test on tree with two nodes
   @Test
   public void testFindPreviousOnTwoNodeTree() {
     tree = new BinarySearchTree<Record>();
@@ -104,9 +114,9 @@ public class BinarySearchTreeTest {
     tree.add(new Record("g"));
     Record next = tree.findPrevious(new Record("g"));
     assertEquals(new Record("a").key, next.key);
-  }  
-  
-  //test on tree with two nodes
+  }
+
+  // test on tree with two nodes
   @Test
   public void testFindNextOnTwoNodeTree() {
     tree = new BinarySearchTree<Record>();
@@ -114,92 +124,92 @@ public class BinarySearchTreeTest {
     tree.add(new Record("g"));
     Record next = tree.findNext(new Record("a"));
     assertEquals(new Record("g").key, next.key);
-  } 
+  }
 
-  //d
-  @Test(expected=NoSuchElementException.class)
+  // d
+  @Test(expected = NoSuchElementException.class)
   public void testFindNonExistingPrevious() {
     tree.findPrevious(new Record("d"));
   }
-  
-  //d
+
+  // d
   @Test
   public void testFindNextAsParent() {
     Record next = tree.findNext(new Record("d"));
     assertEquals(new Record("g").key, next.key);
   }
-  
-  //g
+
+  // g
   @Test
   public void testFindNext() {
     Record next = tree.findNext(new Record("g"));
     assertEquals(new Record("i").key, next.key);
   }
-  
-  //g
+
+  // g
   @Test
   public void testFindPreviousAsLeftChild() {
     Record next = tree.findPrevious(new Record("g"));
     assertEquals(new Record("d").key, next.key);
   }
-  
-  //i
+
+  // i
   @Test
   public void testFindNextAsParentI() {
     Record next = tree.findNext(new Record("i"));
     assertEquals(new Record("j").key, next.key);
   }
-  
-  //i
+
+  // i
   @Test
   public void testFindPreviousAsAncestor() {
     Record next = tree.findPrevious(new Record("i"));
     assertEquals(new Record("g").key, next.key);
   }
-  
-  //j
+
+  // j
   @Test
   public void testFindNextAsRightChild() {
     Record next = tree.findNext(new Record("j"));
     assertEquals(new Record("k").key, next.key);
   }
-  
-  //j
+
+  // j
   @Test
   public void testFindPreviousAsLeftChildJ() {
     Record next = tree.findPrevious(new Record("j"));
     assertEquals(new Record("i").key, next.key);
   }
-  
-  //k
+
+  // k
   @Test
   public void testFindNextThatIsADistantAncestor() {
     Record next = tree.findNext(new Record("k"));
     assertEquals(new Record("m").key, next.key);
   }
-  
-  //k
+
+  // k
   @Test
   public void testFindPreviousOnRightChildWithNoChildren() {
     Record next = tree.findPrevious(new Record("k"));
     assertEquals(new Record("j").key, next.key);
   }
-  
-  //m
-  @Test(expected=NoSuchElementException.class)
+
+  // m
+  @Test(expected = NoSuchElementException.class)
   public void testFindNextOnRoot() {
     Record next = tree.findNext(new Record("m"));
     System.out.println(next);
   }
-  
-  //m
+
+  // m
   @Test
   public void testFindPreviousOnRoot() {
     Record next = tree.findPrevious(new Record("m"));
     assertEquals(new Record("k").key, next.key);
   }
-  
-  //z
+
+  // z
   @Test
   public void testFindNextOnNoTargetRecord() {
     Record next = tree.findNext(new Record("z"));
