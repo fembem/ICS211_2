3aec16dd2599da19147dd690d22dd713d489fdda
fembem <leo.decandia@gmail.com>
2011-11-19 09:35:18 -1000

    added methods to minheap class.

--- ICS211HW11/src/MinHeap.java
+++ ICS211HW11/src/MinHeap.java
@@ -19,14 +19,15 @@ public class MinHeap<E extends Comparable<E>> {
   }
   
   private void heapifyUp(int index) {
-    if(index <= 0) return;
-    int parentIndex = getParentIndex(index);
-    E parentObject = (E)data[parentIndex];
-    E thisObject = (E)data[index];
-    if (thisObject.compareTo(parentObject) < 0) {
-      swapElements(index, parentIndex);
+    while (index > 0) {
+      int parentIndex = getParentIndex(index);
+      E parentObject = (E)data[parentIndex];
+      E thisObject = (E)data[index];
+      if (thisObject.compareTo(parentObject) < 0) {
+        swapElements(index, parentIndex);
+        index = parentIndex;
+      } else break;
     }
-    heapifyUp(parentIndex);
   }
 
   private int getParentIndex(int index) {
@@ -51,7 +52,8 @@ public class MinHeap<E extends Comparable<E>> {
      if (maxIndex >= 0) {
        E result = (E)data[maxIndex];
        maxIndex--;
-       heapifyDown();
+       data[0] = result;
+       heapifyDown(0);
        return result;
      }
      else {
@@ -59,9 +61,57 @@ public class MinHeap<E extends Comparable<E>> {
      }
   }
 
-  private void heapifyDown() {
-    // TODO Auto-generated method stub
+  private void heapifyDown(int index) {
+    if (index >= maxIndex) return;
+    E thisObject = (E)data[index];
+    int leftIndex = getLeftChildIndex(index);
+    int rightIndex = getRightChildIndex(index);
     
+    //is there a left child?  there can be now right child without a left child
+    if (leftIndex > maxIndex) return;
+    E leftObject = (E)data[leftIndex];
+    int orderLeft = thisObject.compareTo(leftObject);
+    
+    //start out assuming the left child is the smaller child index
+    //there may not even be a right child
+    int smallerChildIndex = leftIndex;
+    
+    //given that there is a left child index, is there a right child index too?
+    if (rightIndex <= maxIndex) {
+      E rightObject = (E)data[rightIndex];
+      int orderRight = thisObject.compareTo(rightObject);
+      //return if both children are greater or equal
+      if (orderLeft >= 0 && orderRight >=0)
+        return;
+      //find the smaller child index
+      int orderLeftRight = leftObject.compareTo(rightObject);
+      if (orderLeftRight > 0) //the right child is smaller
+        smallerChildIndex = rightIndex;
+    } else if (orderLeft <= 0) {
+      return; //there is no right child and the node is smaller than its left child
+    }
+    
+    swapElements(index, smallerChildIndex);
+    heapifyDown(smallerChildIndex);
+    
+  }
+  
+  private void checkMinHeapProperty(int index) {
+    if (index >= maxIndex) return;
+    int leftIndex = getLeftChildIndex(index);
+    if (leftIndex > maxIndex) return;
+    E thisObject = (E)data[index];
+    E leftObject = (E)data[leftIndex];
+    int orderLeft = thisObject.compareTo(leftObject);
+    assert(orderLeft <= 0);
+    checkMinHeapProperty(leftIndex);
+    
+    int rightIndex = getRightChildIndex(index);
+    if (leftIndex > maxIndex) return;
+    E rightObject = (E)data[rightIndex];
+    int orderRight = thisObject.compareTo(rightObject);
+    assert(orderRight <= 0); 
+    checkMinHeapProperty(rightIndex);
   }
   
 }
